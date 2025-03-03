e6e1a14004af998c4d3e7c01b30bc59d1b116cdc
SmartDever02 <smart.jamesjin@gmail.com>
2025-03-03 00:36:44 -0800

    feat: save scores

--- app/api/payload/[htmlIndex]/route.ts
+++ app/api/payload/[htmlIndex]/route.ts
@@ -6,6 +6,14 @@ export async function GET(
   { params }: { params: { htmlIndex: string } }
 ) {
   try {
+    const apiKey = request.headers.get('api-key')
+    const SECRET_KEY = process.env.API_TOKEN
+
+    // Validate API key
+    if (!apiKey || apiKey !== SECRET_KEY) {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+    }
+
     const htmlIndex = params.htmlIndex
     const payloads = await prisma.payloads.findMany({
       where: {
@@ -13,7 +21,11 @@ export async function GET(
       },
     })
 
-    return NextResponse.json({ success: true, length: payloads.length, data: payloads })
+    return NextResponse.json({
+      success: true,
+      length: payloads.length,
+      data: payloads,
+    })
   } catch (error) {
     console.error('Error getting html payloads for htmlIndex:', error)
     return NextResponse.json(
---
+++ app/api/scores/[htmlIndex]/route.ts
@@ -0,0 +1,66 @@
+import { NextResponse } from 'next/server'
+import prisma from '@/lib/prisma'
+import { z } from 'zod'
+
+const Schema = z.object({
+  ground_html_index: z.number(),
+  accuracy_scores: z.array(z.number()),
+  seo_scores: z.array(z.number()),
+})
+
+export async function POST(
+  request: Request,
+  { params }: { params: { htmlIndex: string } }
+) {
+  try {
+    const jsonData = await request.json()
+    const parsedData = Schema.safeParse(jsonData)
+
+    if (!parsedData.success) {
+      return NextResponse.json(
+        { error: parsedData.error.format() },
+        { status: 400 }
+      )
+    }
+
+    const apiKey = request.headers.get('api-key')
+    const SECRET_KEY = process.env.API_TOKEN
+
+    // Validate API key
+    if (!apiKey || apiKey !== SECRET_KEY) {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+    }
+
+    const { ground_html_index, accuracy_scores, seo_scores } = parsedData.data
+
+    const item = await prisma.scores.upsert({
+      where: {
+        groundTruthHtmlIndex_htmlIndex: {
+          groundTruthHtmlIndex: ground_html_index,
+          htmlIndex: Number(params.htmlIndex)
+        }
+      },
+      update: {
+        visualScores: accuracy_scores,
+        lighthouseScores: seo_scores
+      },
+      create: {
+        groundTruthHtmlIndex: ground_html_index,
+        htmlIndex: Number(params.htmlIndex),
+        visualScores: accuracy_scores,
+        lighthouseScores: seo_scores
+      }
+    });
+
+    return NextResponse.json({
+      success: true,
+      item,
+    })
+  } catch (error) {
+    console.error('Error getting html payloads for htmlIndex:', error)
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 }
+    )
+  }
+}
