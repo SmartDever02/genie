5b1318cca95c40de50fe4fae412291f22ac5eea2
SmartDever02 <smart.jamesjin@gmail.com>
2025-03-06 09:13:29 -0800

    feat: button width

--- app/dashboard/section.scores.tsx
+++ app/dashboard/section.scores.tsx
@@ -61,7 +61,7 @@ export default function Scores({
         </span>
       </p>
       <hr className="my-3 border-dashed border-gray-400" />
-      <ul className="max-h-[600px] overflow-y-auto">
+      <ul className="max-h-[600px] overflow-y-auto mb-5">
         {scores.map((item) => {
           const map = maps.find(
             (element) => element.htmlIndex === item.htmlIndex
@@ -105,7 +105,7 @@ export default function Scores({
 
       <button
         onClick={getMissingIndexHandler}
-        className="disabled:bg-slate-600 bg-blue-500 hover:bg-blue-400 text-white py-1.5 px-4 rounded text-sm"
+        className="w-full disabled:bg-slate-600 bg-blue-500 hover:bg-blue-400 text-white py-1.5 px-4 rounded text-sm"
       >
         Get Missing Indexes to score
       </button>
