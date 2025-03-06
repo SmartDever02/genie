713c7f70f1bce0f3e7940bfa4d2b2fd3dbcd82a9
SmartDever02 <smart.jamesjin@gmail.com>
2025-03-06 06:35:18 -0800

    feat: generate map

--- app/api/map/route.ts
+++ app/api/map/route.ts
@@ -2,14 +2,36 @@ import { NextResponse } from 'next/server'
 import prisma from '@/lib/prisma'
 import { generateScoreMap } from '@/service/generate-score-map'
 import { ChallengeType } from '@prisma/client'
+import { z } from 'zod'
 
-export async function POST() {
+export const maxDuration = 300;
+
+const Schema = z.object({
+  min: z.number(),
+  max: z.number(),
+})
+
+export async function POST(req: Request) {
   try {
+    const jsonData = await req.json()
+    const parsedData = Schema.safeParse(jsonData)
+
+    if (!parsedData.success) {
+      return NextResponse.json(
+        { error: parsedData.error.format() },
+        { status: 400 }
+      )
+    }
+
+    const { min, max } = parsedData.data
+
     const scores = await prisma.scores.findMany({
-      orderBy: {
-        'updatedAt': 'asc'
+      where: {
+        htmlIndex: {
+          gte: min,
+          lte: max,
+        },
       },
-      take: 300
     })
 
     Promise.all(
@@ -20,7 +42,7 @@ export async function POST() {
 
     return NextResponse.json({
       success: true,
-      length: scores.length
+      length: scores.length,
     })
   } catch (error) {
     console.error('Error getting html payloads for htmlIndex:', error)
--- components/client.score.tsx
+++ components/client.score.tsx
@@ -1,15 +1,25 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 import toast from 'react-hot-toast'
 
 export default function CalculateAll() {
+  const [range, setRange] = useState<string>('')
+  const [isPending, setIsPending] = useState<boolean>(false)
   const router = useRouter()
 
   const handler = async () => {
     try {
+      const [min, max] = range.split(',').map((item) => Number(item))
+
+      setIsPending(true)
       const res = await fetch(`/api/map`, {
         method: 'POST',
+        body: JSON.stringify({
+          min,
+          max,
+        }),
       })
       if (!res.ok) {
         throw new Error(`API request failed with status ${res.status}`)
@@ -22,16 +32,29 @@ export default function CalculateAll() {
     } catch (e) {
       toast.error('Failed to re-calculate scores to generate maps')
     } finally {
+      setIsPending(false)
+      setRange('')
       router.refresh()
     }
   }
 
   return (
-    <button
-      onClick={handler}
-      className="mt-auto disabled:bg-slate-600 bg-blue-500 hover:bg-blue-400 text-white py-1.5 px-4 rounded text-sm w-full"
-    >
-      Calculate Maps [ALL]
-    </button>
+    <div className="w-full flex flex-col gap-2">
+      <input
+        disabled={isPending}
+        placeholder="min,max"
+        className="px-2 bg-white/10 rounded-md w-full"
+        value={range}
+        onChange={(e) => setRange(e.target.value)}
+        type="text"
+      />
+      <button
+        disabled={isPending}
+        onClick={handler}
+        className="mt-auto disabled:bg-slate-600 bg-blue-500 hover:bg-blue-400 text-white py-1.5 px-4 rounded text-sm w-full"
+      >
+        {isPending ? 'Pending...' : 'Calculate Maps [ALL]'}
+      </button>
+    </div>
   )
 }
